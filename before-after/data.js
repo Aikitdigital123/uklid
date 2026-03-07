e7de618a29580a88caaa1849c75290e15fb63cb4
Aikitdigital123 <Aikitdigital123@users.noreply.github.com>
2026-03-07 21:56:26 +0000

    deploy: e0a56212ae44fe3d323dd5ffc1e0d6f8e4a8ac8c

---
+++ before-after/data.js
@@ -0,0 +1,23 @@
+window.beforeAfterGallery = [
+  {
+    title: "Úklid",
+    date: "2025-01-03",
+    before: "/before-after/photos/2025-01-03-pred.jpeg",
+    after: "/before-after/photos/2025-01-03-po.jpeg",
+    note: ""
+  },
+  {
+    title: "Úklid",
+    date: "2025-11-23",
+    before: "/before-after/photos/2025-11-23-pred.jpg",
+    after: "/before-after/photos/2025-11-23-po.jpg",
+    note: ""
+  },
+  {
+    title: "Úklid",
+    date: "2026-02-14",
+    before: "/before-after/photos/2026-02-14-pred.jpeg",
+    after: "/before-after/photos/2026-02-14-po.jpeg",
+    note: ""
+  }
+];
